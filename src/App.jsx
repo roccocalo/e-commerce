0c575acbd578905700a57ac99136f08052ebe574
roccocalo <roccocalo03@gmail.com>
2024-03-13 19:57:35 +0100

    Setup React in Vite with Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  
+  return (
+    
+    <h1 className="text-3xl font-bold text-center text-red-600 underline">
+    Hello world!
+  </h1>
+  )
+}
+
+export default App
